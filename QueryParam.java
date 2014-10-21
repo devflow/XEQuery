0e739ca38b5970d6c01e7e8193dcca330f06270a
devflow <admin@devflow.kr>
2014-10-21 11:36:51 +0900

    Create QueryParam.java

---
+++ QueryParam.java
@@ -0,0 +1,17 @@
+/*
+ * this is privacy project.
+ * not applied any copyrights yet.
+ * - author: admin@devflow.kr
+ */
+
+package **INSERT_YOUR_PACKAGE**;
+
+public class QueryParam {
+    String key;
+    String val;
+
+    public QueryParam(String k, String v) {
+        key = k;
+        val = v;
+    }
+}
