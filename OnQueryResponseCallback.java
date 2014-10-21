5616c7b624085d15f6e9f6ea61287d1dcddd3f85
devflow <admin@devflow.kr>
2014-10-21 11:36:06 +0900

    Create OnQueryResponseCallback.java

---
+++ OnQueryResponseCallback.java
@@ -0,0 +1,11 @@
+/*
+ * this is privacy project.
+ * not applied any copyrights yet.
+ * - author: admin@devflow.kr
+ */
+
+package **INSERT_YOUR_PACKAGE**;
+
+public interface OnQueryResponseCallback {
+    void onResponse(ResponseQueryData data);
+}
